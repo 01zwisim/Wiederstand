0f2f8e5ccc9a234ebfe26d8a2767304d27354cf9
Zwischenbrugger <01ZwiSim>
2018-04-16 09:05:24 +0200

    Klasse gescchrieben set methoden fehlt noch die berechnung

---
+++ Wiederstandsrechner/Wiederstandsrechner/WiederstandBerechnung.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class WiederstandBerechnung
+    {
+        private int r1;
+        private int r2;
+        private bool reihe;
+
+        public void setR1(int wert)
+        {
+            r1 = wert;                                  
+        }
+        public void setR2(int wert)
+        {
+            r2 = wert;
+        }
+        public void Reihe(bool wert)
+        {
+            this.reihe = wert;
+        }
+
+    }
+}
